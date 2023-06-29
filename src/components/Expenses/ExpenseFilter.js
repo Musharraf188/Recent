d4d8bc01b8eb647b2614bee0b53466a612b1110e
Musharraf188 <mr.musharraf188@gmail.com>
2023-06-29 21:00:59 +0530

    ExpenseFilter/Added

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,23 @@
+
+import './ExpenseFilter.css';
+
+const ExpenseFilter =(props)=>{
+    const dropDownHandler =(event)=>{
+props.onFilter(event.target.value);
+console.log(event.target.value)
+    }
+    return <div className='expenses-filter'>
+          <div className='expenses-filter__control'>
+        <label>Search by year.</label>
+        <select onChange={dropDownHandler}>
+            <option value={2023}>2023</option>
+            <option value={2022}>2022</option>
+            <option value={2021}>2021</option>
+            <option value={2020}>2020</option>
+        </select>
+        </div>
+       
+    </div>
+}
+
+export default ExpenseFilter;
