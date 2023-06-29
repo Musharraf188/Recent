a37e0f8e1c51fa52385c56bc0038dda5e68991ac
Musharraf188 <mr.musharraf188@gmail.com>
2023-06-29 19:20:48 +0530

    EpenseForm/Added

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -1,8 +1,15 @@
 import ExpenseForm from '../Expenses/ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = ()=>{
-    return<div className='new-expense'> <ExpenseForm /></div>
+const NewExpense = (props)=>{
+    const dataFromUser =(userInput)=>{
+const expenseData={
+    ...userInput
+}
+props.onAdd(expenseData)
+}
+    return<div className='new-expense'> 
+    <ExpenseForm onSaveData={dataFromUser}/></div>
 }
 
 export default NewExpense;
