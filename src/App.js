c2d06c8c5f9547e0c8ce67fcf49ffcc3f811e245
Musharraf188 <mr.musharraf188@gmail.com>
2023-06-29 16:58:23 +0530

    ExppenseDate/Added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
 
 const App =()=>{
+  // Array of object for dummy expense
+  const expenses=[
+    {id:'e1',date:new Date(2020, 11, 12), title:'Car Insurance', amount:237.98 },
+    {id:'e2',date:new Date(2021, 8, 12), title:'Home Expenses', amount:237.98 },
+    {id:'e3',date:new Date(2022, 6, 12), title:'Personal Expenses', amount:237.98 },
+    {id:'e4',date:new Date(2023, 2, 12), title:'Flue Expenses', amount:237.98 }
+  ]
   return <div> <h2>Hello World</h2>
-  <ExpenseItem />
+  <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} />
+  <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} />
+  <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} />
+  <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} />
   </div>
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 const ExpenseItem = (props) => {
+    console.log(props.date)
     // Wrapper Component
     return (
         <div className='expense-item '>
+           
     <ExpenseDate date={props.date}/>
             <div className='expense-item__description'><h2>{props.title}</h2>
             <div className='expense-item__price'>{props.amount}</div></div>
