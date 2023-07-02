35d73105d18fd3f7f802426f7411c8eff5216b8f
Musharraf188 <mr.musharraf188@gmail.com>
2023-07-02 10:08:08 +0530

    ExpesesList

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import Card from '../UI/Card';
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
+import ExpenseList from './ExpenseList';
 import ExpenseFilter from './ExpenseFilter';
 import { useState } from 'react';
 
@@ -12,15 +12,10 @@ setFilterYear(selectedYear);
   const filterExpense = props.expenses.filter(expense=>{
     return expense.date.getFullYear() .toString()=== filterYear
   })
-    let expenseContent = <p>No Expenses Found.</p>
-    if(props.expenses.length> 0){
-      expenseContent=filterExpense.map(expense=>
-        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
-      )
-    }
+    
     return <Card className='expenses '>
       <ExpenseFilter onFilter={onChange} />
-      {expenseContent}
+     <ExpenseList items={filterExpense}/>
          {/* <ExpenseItem date={props.expenses[0].date} title={props.expenses[0].title} amount={props.expenses[0].amount} />
   <ExpenseItem date={props.expenses[1].date} title={props.expenses[1].title} amount={props.expenses[1].amount} />
   <ExpenseItem date={props.expenses[2].date} title={props.expenses[2].title} amount={props.expenses[2].amount} />
